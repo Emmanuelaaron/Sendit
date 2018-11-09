9cf8d071426ee3c7ec2c0523a9da092b9801dbb8
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2018-11-09 07:45:53 +0300

    Getting a specific product This modifies the get method Feature enables user to get a specific prodcut [Delivers #161824167]

--- api/models.py
+++ api/models.py
@@ -1,14 +1,16 @@
 users = {
-    "Emma":{
+    1:{
+        "name": "Emma",
         "email": "emmanuelisabirye9@gamil.com",
         "password": "isabirye",
-        "userId": 1
     }
 }
 
+
+
 orders = {
-    "Emma": {
-        "parcel 1":{
+    2: {
+        1:{
             "item": "blanket",
             "pickup_location": "Kansaga",
             "destination": "muyenga",
@@ -35,21 +37,25 @@ class Users:
 
     def signup(self):
         for dict_product in users:
-            if dict_product is self.name:
-                return "Name already taken"
+            if users[dict_product]["name"] is self.name:
+               return "Username already in use! Please use another name"
             else:
-                users[self.name] = {"email":self.email, "password": self.password, "userId":(len(users)+1)}
-                return f"{self.name}, congrats! You have sucessfully signed up"
-    
+                users[len(users)]["name"] = self.name
+                users[len(users)]["email"] = self.email
+                users[len(users)]["password"] = self.password
+                return "You have sucessfully signed up"
+
+
+            
     def login(self, name, password):
         for dict_product in users:
-            if dict_product is not name:
+            if users[dict_product]["name"] is not name:
                 return "Not registered user, please signup" 
+            if users[dict_product]["password"] is not password:
+                return "Incorrect password"
             else:
-                if dict_product is name and users[dict_product]["password"] is password:
-                    return "sucessfully logged in"
-                else:
-                    return "Incorrect password"
+                return "sucessfully logged in"
+
 
 class Admin(Users):
     def __init__(self, name, email, password, user_type):
@@ -76,4 +82,10 @@ class Order:
     def get_all_orders(self):
         return orders
 
-        
+    def get_specific_order(self, parcelId):
+        for order_dict in orders:
+            for parcel_number in orders[order_dict]:
+                if parcel_number is parcelId:
+                    return orders[order_dict][parcel_number]
+            
+
